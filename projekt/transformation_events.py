a506f4c82fe3fe7f13cb8ccd44877db7b62bafee
Hujnis <tomas.hujnak@gmail.com>
2024-04-29 12:10:58 +0200

    +

--- projekt/transformation_events.py
+++ projekt/transformation_events.py
@@ -3,7 +3,7 @@ import json
 from tabulate import tabulate
 
 # Načtení dat ze souboru data.json
-with open('C:/Users/thujn/OneDrive - Univerzita obrany/Plocha/UNOB/Semestr 8/projekt/data.json', 'r') as file:
+with open('C:/Users/thujn/OUISproject/projekt/data.json', 'r') as file:
     data = json.load(file)
 
 # Vytvoření pandas DataFrame z eventPage
@@ -13,7 +13,7 @@ df = pd.DataFrame(data['data']['eventPage'])
 df.columns = ['id', 'name', 'place', 'startdate', 'enddate']
 
 # Uložení kontingenční tabulky do souboru
-with open('C:/Users/thujn/OneDrive - Univerzita obrany/Plocha/UNOB/Semestr 8/projekt/contingency_table.json', 'w') as file:
+with open('C:/Users/thujn/OUISproject/projekt/contingency_table.json', 'w') as file:
     file.write(tabulate(df, headers='keys', tablefmt='pipe', showindex=False))
 
 print("Kontingenční tabulka byla uložena do souboru 'contingency_table.json'.")
